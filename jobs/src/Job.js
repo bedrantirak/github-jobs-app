cecc65f1075202b6db355ada916cef9b69963960
bedrantirak <bdrntrk@gmail.com>
2020-12-17 13:57:20 +0300

    Update Job.js

--- jobs/src/Job.js
+++ jobs/src/Job.js
@@ -1,12 +1,14 @@
- import React from 'react'
- import {Card,Badge} from "react-bootstrap"
+ import React, { useState } from 'react'
+ import {Card,Badge,Button,Collapse} from "react-bootstrap"
  import ReactMarkdown from "react-markdown"
  
  export default function Job({job }) {
+     const [open, setOpen] = useState(false);
      return (
-         <Card>
+         <Card className="mb-3">
              <Card.Body>
                  <div className="d-flex justify-content-between">
+
                      <div >
                          <Card.Title>
                          {job.title} - <span className="text-muted font-weight-light">
@@ -17,7 +19,7 @@
                              {new Date(job.created_at).toLocaleDateString() }
                          </Card.Subtitle>
                          <Badge variant="secondary" className="mr-2" >{job.type} </Badge>
-                         <Badge variant="secondary">{job.location} </Badge>
+                         <Badge variant="secondary" >{job.location} </Badge>
                          <div style={{wordBreak: 'break-all'}}>
                          <ReactMarkdown source={job.how_to_apply} />
                          </div>
@@ -25,6 +27,19 @@
                      </div> 
                     <img className="d-none d-md-block" height="50" alt={job.company} src={job.company_logo} />
                  </div>
+                 <Card.Text>
+                     <Button 
+                     onClick={() => setOpen(prevOpen => !prevOpen )} 
+                     variant="primary"
+                     >
+                        {open ? "Hide Details" : "View Details"} </Button>
+                 </Card.Text>
+                 <Collapse in={open}>
+                 <div className="mt-4">
+                     <ReactMarkdown source={job.description} />
+                 </div>
+                 </Collapse>
+                 
              </Card.Body>             
          </Card>
      )
